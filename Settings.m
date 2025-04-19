f78df58a7b6020fae098d723ecaf42960fda0a2d
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-19 13:26:47 +0800

    Update Settings.m

--- Settings.m
+++ Settings.m
@@ -1,10 +1,11 @@
 #import "Settings.h"
+#import <UIKit/UIKit.h>
 #import <sys/sysctl.h>
 
 @implementation Settings
 
 // 获取设备 ECID
-+ (NSString *)getECID {
+- (NSString *)getECID {
     NSString *ecid = nil;
     FILE *pipe = popen("ioreg -d2 -c AppleMobileApNonce -a | plutil -extract ECID xml1 -o - - | plutil -p -", "r");
     if (pipe) {
@@ -24,14 +25,17 @@
 }
 
 // 获取设备型号
-+ (NSString *)getDeviceModel {
+- (NSString *)getDeviceModel {
     size_t size;
     sysctlbyname("hw.model", NULL, &size, NULL, 0);
     char *model = (char *)malloc(size);
-    sysctlbyname("hw.model", model, &size, NULL, 0);
-    NSString *result = [NSString stringWithUTF8String:model];
-    free(model);
-    return result ?: @"未知型号";
+    if (model) {
+        sysctlbyname("hw.model", model, &size, NULL, 0);
+        NSString *result = [NSString stringWithUTF8String:model];
+        free(model);
+        return result ?: @"未知型号";
+    }
+    return @"未知型号";
 }
 
 @end
