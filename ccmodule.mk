04adf5883ee20039ec3eec3497c946c1e07ffe64
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-19 11:24:23 +0800

    Delete ccmodule.mk

--- ccmodule.mk
+++
@@ -1,6 +0,0 @@
-# Control Center 模块定义
-CCMODULE_NAME = SHSHHelperCC
-SHSHHelperCC_FILES = SHSHHelperCCModule.xm
-SHSHHelperCC_FRAMEWORKS = UIKit ControlCenterUIKit
-
-include $(THEOS_MAKE_PATH)/ccmodule.mk   # ✅ 引用 Theos 自带的规则
